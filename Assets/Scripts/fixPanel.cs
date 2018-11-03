1e72059d62884293a717859f3ed73a17eae61fb2
Shawn Oakley <shawnoakley@gmail.com>
2018-11-03 17:39:05 -0400

    Adds stylized menu/animations/audio source

---
+++ Assets/Scripts/fixPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class fixPanel : MonoBehaviour {
+
+	private string s = "";
+	private bool onOff = false;
+	// Use this for initialization
+	void Start () {
+		s = SceneManager.GetActiveScene().name;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void toggleFlash(){
+		if (onOff==false) {
+			CameraDevice.Instance.SetFlashTorchMode(true);
+			onOff = true;
+		} else {
+			CameraDevice.Instance.SetFlashTorchMode(false);
+			onOff = false;
+		}
+	}
+
+	public void refresh(){
+		SceneManager.LoadScene(s);
+	}
+
+}
