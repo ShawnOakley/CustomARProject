68baaf4c84a7258d708a157e8ae9133c4d2af516
Shawn Oakley <shawnoakley@gmail.com>
2018-11-04 22:52:28 -0500

    Finalizes high score animation. Begins work on dance selection

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour {
+
+	public GameObject HSPanel;
+	private Animator anim;
+	public Text HSText;
+
+	// Use this for initialization
+	void Start () {
+		anim = HSPanel.GetComponent<Animator>();
+		gameOver();
+		getHighScore();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void playGame(){
+		SceneManager.LoadScene("main");
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+
+	public void playHSPanel(){
+		anim.SetTrigger("playHSAnim");
+	}
+
+	public void backHSPanel(){
+		anim.SetTrigger("backHSAnim");
+	}
+
+	public void gameOver(){
+		// to be continued
+		PlayerPrefs.SetString("HighScore","52345");
+	}
+
+	private void getHighScore(){
+		HSText.text = PlayerPrefs.GetString("HighScore");
+	}
+}
---
+++ Assets/Scripts/spawnerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SpawnProcess(){
+		InvokeRepeating("spawn", 2.5f, 1.5f);
+	}
+
+	void spawn(){
+		GameObject x = (GameObject).Instantiate(Resources.Load("pointButton"));
+		x.transform.parent = transform;
+	}
+}
