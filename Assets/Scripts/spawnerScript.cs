ccc6473bb6f5596477d202f51661064c9de00c3a
Shawn Oakley <shawnoakley@gmail.com>
2018-11-06 03:00:16 -0500

    Adds multiple animation transitions

--- Assets/Scripts/spawnerScript.cs
+++ Assets/Scripts/spawnerScript.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class spawner : MonoBehaviour {
+public class spawnerScript : MonoBehaviour {
 
+	public Text myScore;
+	public static spawnerScript instance;
+	private GameObject dancer;
+	private Animator anim;
 	// Use this for initialization
 	void Start () {
+ 		SpawnProcess();
+	}
 
+	void Awake() {
+		if(instance==null){
+			instance = this;
+		}
 	}
 
 	// Update is called once per frame
@@ -19,7 +30,28 @@ public class spawner : MonoBehaviour {
 	}
 
 	void spawn(){
-		GameObject x = (GameObject).Instantiate(Resources.Load("pointButton"));
+		GameObject x = (GameObject)Instantiate(Resources.Load("pointButton"));
 		x.transform.parent = transform;
+
+		RectTransform position = x.GetComponent<RectTransform>();
+		position.localPosition = new Vector3(0,60, 0);
+		position.localScale = new Vector3(1,1,1);
+	}
+
+	public void addScore(int theScore){
+		myScore.text = (int.Parse(myScore.text) + theScore).ToString();
+		removeKids ();
+	}
+
+	private void removeKids(){
+		foreach(Transform child in transform) {
+			Destroy (child.gameObject);
+		}
+	}
+
+	public void makeMove(string danceMove) {
+		dancer = GameObject.Find ("UserDefinedTarget-1/mremireh_o_desbiens");
+		anim = dancer.GetComponent<Animator> ();
+		anim.SetTrigger(danceMove);
 	}
 }
